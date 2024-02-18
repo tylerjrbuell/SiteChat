a760b96ad5e84f251bd52fe61c3a1d0bdbb996e2
tyler.buell <tyler.buell@sparkbusinessworks.com>
2024-02-17 21:47:59 -0500

    fix: site sync to use puppeteer instead of selenium webdriver

--- src/site-sync/helpers/index.ts
+++ src/site-sync/helpers/index.ts
@@ -1,42 +1,90 @@
 const { writeFile } = require('fs').promises
-const { By } = require('selenium-webdriver')
 const puppeteer = require('puppeteer')
 
-async function savePageText(driver: any, url: string, path: string = './data') {
+async function savePageText(
+  page: any,
+  url: string = '',
+  path: string = './data'
+) {
   try {
-    await driver.get(url)
-    const pageTitle = await driver.getTitle()
-    // console.log(`Processing ${url} -> ${pageTitle}`);
-    const fileName = pageTitle.replace(/[/\\?%*:|"<>]/g, '') // Remove invalid characters from the title
-    let body = await driver.findElement(By.tagName('body'))
-    const pageText = `Page Source: ${url}\n${await body.getText()}`
+    await page.goto(url)
+    const pageTitle = await page.title()
+    const fileName = pageTitle
+      ? pageTitle.replace(/[/\\?%*:|"<>]/g, '')
+      : url
+          .split('://')
+          .pop()
+          .replace(/[/\\?%*:|"<>]/g, '-') // Remove invalid characters from the title
+    const pageText = `Page Source: ${url}\n${await page.evaluate(
+      () => document.body.innerText
+    )}`
     await writeFile(`${path}/${fileName}.txt`, pageText)
     // console.log(`Saved ${fileName}.txt`);
   } catch (error) {
     console.error(`Error occurred while processing ${url}`, error)
   }
 }
-async function savePageAsPDF(driver: any, url: string, path: string) {
+async function savePageAsPDF(page: any, url: string, path: string) {
   try {
-    await driver.get(url)
-    const pageTitle = await driver.getTitle()
-    // console.log(`Processing ${url} -> ${pageTitle}`);
+    await page.goto(url)
+    const pageTitle = await page.title()
     const fileName = pageTitle.replace(/[/\\?%*:|"<>]/g, '')
-    const htmlContent = await driver.getPageSource()
+    const htmlContent = await page.source()
     const puppetBrowser = await puppeteer.launch({
       executablePath: '/usr/bin/google-chrome',
       headless: true,
       userDataDir: `${__dirname}/chrome-profile`,
     })
-    const page = await puppetBrowser.newPage()
     await page.setContent(htmlContent)
     const pdfBuffer = await page.pdf({ format: 'letter', scale: 1 })
     await writeFile(`${path}/${fileName}.pdf`, pdfBuffer)
-    // console.log(`Saved ${fileName}.pdf`);
+    console.log(`Saved ${fileName}.pdf`)
     await puppetBrowser.close()
   } catch (error) {
     console.error(`Error occurred while processing ${url}`, error)
   }
 }
 
-export { savePageText, savePageAsPDF }
+async function collectPageLinksRecursive(
+  browser: any,
+  baseUrl: string,
+  currentUrl: string = '',
+  collectedUrls: Set<string>,
+  visitedUrls: Set<string>,
+  abortSignal: AbortSignal,
+  limit: number = 10
+) {
+  if (visitedUrls.has(currentUrl) || abortSignal.aborted) return
+  console.log(`Processing: ${currentUrl}`)
+  const page = await browser.newPage()
+  await page.goto(currentUrl)
+  const links = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll('a')).map((link) => link.href)
+  })
+  visitedUrls.add(currentUrl)
+  for (let link of links) {
+    if ((limit && collectedUrls.size >= limit) || abortSignal.aborted) break
+    const pageUrl = link?.split('#')[0]
+    try {
+      if (pageUrl && pageUrl.includes(baseUrl) && !collectedUrls.has(pageUrl)) {
+        collectedUrls.add(pageUrl)
+        console.log(`Collected ${pageUrl}`)
+        await collectPageLinksRecursive(
+          browser,
+          baseUrl,
+          pageUrl,
+          collectedUrls,
+          visitedUrls,
+          abortSignal,
+          limit
+        )
+      }
+    } catch (error: any) {
+      console.error('An error occurred skipping link', error.message)
+      console.log(pageUrl)
+      continue
+    }
+  }
+}
+
+export { collectPageLinksRecursive, savePageText, savePageAsPDF }
--- src/site-sync/index.ts
+++ src/site-sync/index.ts
@@ -1,8 +1,8 @@
-const { Builder, By, Key, until } = require('selenium-webdriver')
-const { mkdir, stat } = require('fs').promises
+// const { Builder, By, Key, until } = require('selenium-webdriver')
+const puppeteer = require('puppeteer')
+const { mkdir } = require('fs').promises
 const { existsSync } = require('fs')
-const { savePageText } = require('./helpers')
-const chrome = require('selenium-webdriver/chrome')
+const { savePageText, collectPageLinksRecursive } = require('./helpers')
 const cliProgress = require('cli-progress')
 
 // create a new progress bar instance and use shades_classic theme
@@ -11,84 +11,49 @@ const progressBar = new cliProgress.SingleBar(
   cliProgress.Presets.shades_classic
 )
 
-const username = process.env.USERNAME
-const password = process.env.PASSWORD
 const dataDir = process.env.DATA_DIR
-async function syncSiteContent(siteUrl: string = '', singleUrl: boolean = true, abortSignal: AbortSignal) {
- 
+async function syncSiteContent(
+  siteUrl: string = '',
+  singleUrl: boolean = true,
+  abortSignal: AbortSignal
+) {
   const baseUrl = siteUrl || process.env.BASE_URL
   if (!existsSync(dataDir)) await mkdir(dataDir)
-  const urls: Array<string> = []
-  const chromeOptions = new chrome.Options().headless()
-  chromeOptions.addArguments(`user-data-dir=${__dirname}/chrome-profile`)
-  let driver = await new Builder()
-    .forBrowser('chrome')
-    .setChromeOptions(chromeOptions)
-    .build()
+  const urls: Set<string> = new Set()
+  const processedUrls: Set<string> = new Set()
+  const puppetBrowser = await puppeteer.launch({
+    executablePath: '/usr/bin/google-chrome',
+    userDataDir: `${__dirname}/chrome-profile`,
+  })
   try {
-    await driver.get(baseUrl)
-    // if (!(await driver.getCurrentUrl()).startsWith(baseUrl)) {
-    //   console.log('Login is required, please approve the 2FA login prompt.')
-    //   await driver
-    //     .findElement(By.name('identifier'))
-    //     .sendKeys(username, Key.RETURN)
-    //   // Wait until the password field is visible and interactable
-    //   await driver.wait(until.elementLocated(By.name('Passwd')), 10000)
-    //   await driver.wait(
-    //     until.elementIsVisible(driver.findElement(By.name('Passwd'))),
-    //     10000
-    //   )
-    //   await driver.wait(
-    //     until.elementIsEnabled(driver.findElement(By.name('Passwd'))),
-    //     10000
-    //   )
-
-    //   // Scroll the password field into view if necessary
-    //   const passwordField = await driver.findElement(By.name('Passwd'))
-    //   await driver.executeScript(
-    //     'arguments[0].scrollIntoView(true);',
-    //     passwordField
-    //   )
-    //   await passwordField.sendKeys(password, Key.RETURN)
-    //   // Perform 2-factor authentication manually
-    //   // Wait for the user to complete 2-factor authentication
-    //   // Once 2-factor authentication is completed, navigate to the desired page
-    // }
-    console.log('\nLogged in successfully.')
     if (!singleUrl) {
-      // Get all the links on the page
-      const elements = await driver.findElements(By.tagName('a'))
       console.log('Collecting all Site page links...')
-      for (let element of elements) {
-        try {
-          if (abortSignal.aborted) return {
-            success: false,
-            message: 'Site download aborted.',
-          }
-          const href = await element.getAttribute('href')
-          const pageUrl = href.split('#')[0]
-          if (pageUrl && pageUrl.startsWith(baseUrl) && !urls.includes(pageUrl)) {
-            urls.push(pageUrl)
-          }
-        } catch (error) {
-          console.error('An error occurred skipping link')
-        }
-      }
+      await collectPageLinksRecursive(
+        puppetBrowser,
+        baseUrl,
+        baseUrl,
+        urls,
+        processedUrls,
+        abortSignal,
+        25
+      )
     }
-    urls.push(siteUrl)
-    console.log(`Syncing latest Site content (${urls.length} pages)...`)
-    progressBar.start(urls.length, 0)
+    urls.add(siteUrl)
+    console.log(`Syncing latest Site content (${urls.size} pages)...`)
+    progressBar.start(urls.size, 0)
+    const page = await puppetBrowser.newPage()
     // Save each page as text
     for (let url of urls) {
-      if (abortSignal.aborted) return {
-        success: false,
-        message: 'Site download aborted.',
+      if (abortSignal.aborted)
+        return {
+          success: false,
+          message: 'Site download aborted.',
+        }
+      if (!(await page.url()).startsWith(url)) {
+        await page.goto(url)
       }
-      if (!(await driver.getCurrentUrl()).startsWith(url)) {
-        await driver.get(url)
-      }
-      progressBar.update(urls.indexOf(url) + 1)
-      await savePageText(driver, url, dataDir)
+      progressBar.update([...urls].indexOf(url) + 1)
+      await savePageText(page, url, dataDir)
     }
     console.log('\nSuccessfully synced Site content.')
     return {
@@ -105,11 +70,8 @@ async function syncSiteContent(siteUrl: string = '', singleUrl: boolean = true, 
     }
   } finally {
     progressBar.stop()
-    await driver.quit()
   }
 }
-
-
 
 // await syncSiteContent()
 
